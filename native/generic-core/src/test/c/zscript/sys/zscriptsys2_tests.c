cf46a21f98296b7c2df20b48480944ad8852601e
susanw1 <susanwitts1@gmail.com>
2025-03-16 11:21:01 +0000

    support multiple tests

---
+++ native/generic-core/src/test/c/zscript/sys/zscriptsys2_tests.c
@@ -0,0 +1,16 @@
+#include <zscript/sys/zscriptsys.h>
+#include <zscript/all_defs.h>
+
+int main() {
+    #if __STDC_VERSION__ == 201112L
+        printf("C Standard: C11\n");
+    #elif __STDC_VERSION__ == 199901L
+        printf("C Standard: C99\n");
+    #elif __STDC_VERSION__ == 201710L
+        printf("C Standard: C17\n");
+    #else
+        printf("C Standard: Unknown\n");
+    #endif
+    printf("Zscript Version: %s\n", ZSCRIPT_CORE_VERSION);
+    setupZscript();
+}
